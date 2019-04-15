9e2310a9583df29343164c16f8233c146c1629a4
El Mahdi Bentaher <ebentaher@cartaworldwide.com>
2019-04-15 11:10:39 +0100

    CW-335: Change the name of external API

--- Carta.Api.External/Properties/AssemblyInfo.cs
+++ Carta.Api.External/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Carta.External.API")]
+[assembly: AssemblyTitle("Carta.Api.External")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Carta.External.API")]
+[assembly: AssemblyProduct("Carta.Api.External")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
