696f664254cf3f3fbd6f1deafb64e3a0acc7bf72
Bader Elamraoui <belamraoui@cartasolutions.com>
2024-02-07 11:32:41 +0100

    DEV-919: Fix issue merge

--- Carta.Api.External/Service.svc.cs
+++ Carta.Api.External/Service.svc.cs
@@ -297,7 +297,7 @@ namespace Carta.Api.External
                                                  serviceResponse.serviceResponseCode == statusDecline.INVALID_PAN ? DeclineReason.INVALID_PAN :
                                                  serviceResponse.serviceResponseCode == statusDecline.PAN_INELIGIBLE ? DeclineReason.PAN_INELIGIBLE :
                                                  serviceResponse.serviceResponseCode == statusDecline.CVX2_FAILURE ? DeclineReason.CVX2_FAILURE :
-                                                 serviceResponse.serviceResponseCode == statusDecline.CVX2_FAILURE_2 ? DeclineReason.CVX2_FAILURE_2 :
+                                                 serviceResponse.serviceResponseCode == statusDecline.CVX2_FAILURE_2 ? DeclineReason.CVX2_FAILURE :
 
                                                  DeclineReason.OTHER);
             }
@@ -403,7 +403,7 @@ namespace Carta.Api.External
                                                  serviceResponse.serviceResponseCode == statusDecline.INVALID_PAN ? DeclineReason.INVALID_PAN :
                                                  serviceResponse.serviceResponseCode == statusDecline.PAN_INELIGIBLE ? DeclineReason.PAN_INELIGIBLE :
                                                  serviceResponse.serviceResponseCode == statusDecline.CVX2_FAILURE ? DeclineReason.CVX2_FAILURE :
-                                                 serviceResponse.serviceResponseCode == statusDecline.CVX2_FAILURE_2 ? DeclineReason.CVX2_FAILURE_2 :
+                                                 serviceResponse.serviceResponseCode == statusDecline.CVX2_FAILURE_2 ? DeclineReason.CVX2_FAILURE :
                                                  DeclineReason.OTHER);
             }
             log.Info("Final Response :" + outputResponse.ToString());
@@ -437,7 +437,7 @@ namespace Carta.Api.External
                                                  serviceResponse.serviceResponseCode == statusDecline.INVALID_PAN ? DeclineReason.INVALID_PAN :
                                                  serviceResponse.serviceResponseCode == statusDecline.PAN_INELIGIBLE ? DeclineReason.PAN_INELIGIBLE :
                                                  serviceResponse.serviceResponseCode == statusDecline.CVX2_FAILURE ? DeclineReason.CVX2_FAILURE :
-                                                 serviceResponse.serviceResponseCode == statusDecline.CVX2_FAILURE_2 ? DeclineReason.CVX2_FAILURE_2 :
+                                                 serviceResponse.serviceResponseCode == statusDecline.CVX2_FAILURE_2 ? DeclineReason.CVX2_FAILURE :
                                                  DeclineReason.OTHER);
             }
             log.Info("Final Response :" + outpuResponse.ToString());
